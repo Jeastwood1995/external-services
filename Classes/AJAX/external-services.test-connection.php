42782704f1e51ab74928fd1812fcc0429a81b2af
Jack <jeastwood95@gmail.com>
2020-08-30 00:39:11 +0200

    Adding to ajax test connection class

--- Classes/AJAX/external-services.test-connection.php
+++ Classes/AJAX/external-services.test-connection.php
@@ -3,5 +3,13 @@
 
 class AjaxTestConnection
 {
-
+    public function checkConnection() {
+        print_r($_POST);
+        /*
+        foreach ($data as $field) {
+            print_r($field);
+        }
+        */
+        die();
+    }
 }
