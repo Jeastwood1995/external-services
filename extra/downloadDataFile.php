f09eb566f19d74fde4402a273a8ac9d4cdd60d95
Jack Eastwood <jeastwood95@gmail.com>
2021-12-21 17:11:23 +0000

    Adding csrf check in download script and implemented initial data retrieval

--- extra/downloadDataFile.php
+++ extra/downloadDataFile.php
@@ -1,3 +1,25 @@
 <?php
-$hi = 'hi';
+require_once($_SERVER['DOCUMENT_ROOT'] . '/wp-load.php');
 
+//if (!empty($_GET['token']) && wp_verify_nonce($_GET['token'])) {
+	_includePluginClasses();
+	_fetchAndProcessTempData();
+//} else {
+//	die('Unauthorized');
+//}
+
+function _includePluginClasses() {
+	$pluginPath = plugin_dir_path( EXTERNAL_SERVICES_FILE );
+
+	require_once($pluginPath . 'Classes/Setup/external-services.db-setup.php');
+	require_once( $pluginPath . 'Classes/Models/external-services.es-temp-model.php' );
+}
+
+function _fetchAndProcessTempData() {
+	$tempModel = new \ExternalServices\Classes\Models\ES_Temp_Model();
+
+	$result = $tempModel->get()[0];
+	$data = unserialize(base64_decode($result->data));
+
+	$hi = '';
+}
