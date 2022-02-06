33e9a90d8b1f819724d44274ac5a6a43ea3353cd
Jack Eastwood <jeastwood95@gmail.com>
2022-02-06 22:43:29 +0100

    Linked to previous commit

--- js/viewObjects/addService/es-add-service.js
+++ js/viewObjects/addService/es-add-service.js
@@ -1,22 +1,10 @@
-jQuery(function () {
-    let object = new EsAddService();
-    debugger;
-});
-
-class EsAddService {
+class EsAddService extends EsObjectBase {
     constructor() {
-        debugger;
-        this._ajaxConnector = new ServiceConnection();
+        super();
     }
-}
 
-/*
-AddService = function () {
-    debugger;
-    this._ajaxConnector = new ServiceConnection();
-
-    this.displayOrRemoveAuthorizationField = async function (hello) {
-        if ($(this).is(':checked')) {
+    async displayOrRemoveAuthorizationSettingsFields(checkbox) {
+        if (checkbox.is(':checked')) {
             let viewCall = {
                 'action': 'call_view',
                 'view': 'add-service/authHeaderConfigure'
@@ -25,11 +13,65 @@ AddService = function () {
             let result = await this._ajaxConnector.connect(viewCall);
 
             if (result.success === true) {
-                $('#authHeadField').after(result.data);
+                jQuery('#authHeadField').after(result.data);
             }
         } else {
-            $('#authAddForm').length ? $('#authAddForm').remove() : '';
+            jQuery('#authAddForm').length ? jQuery('#authAddForm').remove() : '';
+        }
+    }
+
+    async displayOrRemoveCsvSettingsFields(dropdown) {
+        // if the selected format is csv, then display parsing options e.g. delimeter, escape character
+        if (dropdown.val() === 'csv') {
+            let viewCall = {
+                'action': 'call_view',
+                'view': 'add-service/csvParseSettings'
+            };
+
+            let result = await this._ajaxConnector.connect(viewCall);
+
+            if (result.success === true) {
+                jQuery('#dataFormatField').after(result.data);
+            }
+        } else {
+            jQuery('#csvParseForm').length ? jQuery('#csvParseForm').remove() : '';
+        }
+    }
+
+    async checkConnectionSettings() {
+        // remove the configure service section
+        jQuery('#configure-service-section').remove();
+
+        // If all elements on the form are validated, then make the URL connection via AJAX to the AJAX controller
+        if (jQuery('#add-service').valid()) {
+            try {
+                let result = await this._ajaxConnector.connect(jQuery('#add-service').serializeArray(), /*this._loader*/);
+
+                if (result.success === true && result.data != null) {
+                    await this._displayConfigureDataBox(result);
+                }
+            } catch (e) {
+                console.log("There's been an error: " + e);
+            }
+        }
+    }
+
+    async _displayConfigureDataBox(testConnectionResult) {
+        let viewCall = {
+            'action': 'call_view',
+            'view': 'add-service/configureService',
+            'class': 'ExternalServices\\Classes\\Blocks\\Configure_Service',
+            'data': jQuery.parseJSON(testConnectionResult.data)
+        };
+
+        try {
+            let result = await this._ajaxConnector.connect(viewCall);
+
+            if (result.success === true) {
+                jQuery('#add-service-box').after(result.data);
+            }
+        } catch (e) {
+            console.log("There's been an error: " + e);
         }
     }
 }
-*/
