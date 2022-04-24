1e77a4b3fccf7d65cc4aca0aeb9fef15b909e872
Jack Eastwood <jeastwood95@gmail.com>
2022-04-24 12:30:36 +0200

    Implemented settings box for each field and programmed show/hide switch

--- js/viewObjects/es-configure-service.js
+++ js/viewObjects/es-configure-service.js
@@ -1,7 +1,18 @@
-EsConfigureService = function() {
-    this.massSelectFields = function(selectAll) {
+EsConfigureService = function () {
+    this.massSelectFields = function (selectAll) {
         let mappingCheckboxes = jQuery('#configure-service').find('.data-mappings');
 
         selectAll.is(':checked') ? jQuery(mappingCheckboxes).prop('checked', true) : jQuery(mappingCheckboxes).prop('checked', false);
+    }
+
+    this.showOrHideDataSettingsFields = function(checkbox) {
+        let checkboxId = checkbox.id;
+        let settingsBox = jQuery("tr.data-map-settings." + checkboxId);
+
+        if (jQuery(checkbox).is(':checked')) {
+            settingsBox.css("display", "block");
+        } else {
+            settingsBox.css("display", "none");
+        }
     }
 }
